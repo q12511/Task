01599034b3b4d840f039cac453113dda1dbd3ef8
q12511 <58382876+q12511@users.noreply.github.com>
2021-12-17 21:46:02 +0900

    add delete button

--- index.js
+++ index.js
@@ -10,17 +10,20 @@ function updateButton() {
   var manager = document.getElementById("manager").value;
   var status = "not started yet";
 
+  task.setAttribute("id", "task" + (kazu + 1));
   task.innerHTML = "work " + (kazu + 1) +
   "<br> deadline " + deadline +
   "<br> manager " + manager +
   "<br> status " + status + "<br>";
   button.setAttribute("type", "button");
+  button.setAttribute("id", (kazu + 1));
   button.setAttribute("value", "delete");
-  button.setAttribute("onclick", "updateButton()");
+  button.setAttribute("onclick", "deleteButton(this.id)");
   task.appendChild(button);
   works.appendChild(task);
 }
 
-function deleteButton() {
-
+function deleteButton(id) {
+  var task = document.getElementById("task" + id);
+  task.remove();
 }
